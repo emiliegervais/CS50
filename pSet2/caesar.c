0c801b104a6e2481f3684f4ece02d0434d526b60
Emilie Gervais <eee@emiliegervais.com>
2019-10-08 13:28:11 +0200

    fix: spelling error and handle case argv is empty

--- pSet2/caesar.c
+++ pSet2/caesar.c
@@ -9,47 +9,57 @@
 // argv is an array of string that represents the list of arguments passed
 int main(int argc, string argv[])
 {    
-    // The atoi function converts a string to an integer
-    int k = atoi(argv[1]);
-    // Check if there was a second argument passed with the program call
-    //     and that that second argument is higher than 0 (has to be to cypher with caesar)
-    if (argc == 2 && k > 0)
+    
+    if (argv[1]) 
     {
-        // Prompt user for text to cypher
-        string text = get_string("plaintext: ");
-        // Get length of the string
-        int length = strlen(text);
-        // Create an array of the same length for the cyphered characters
-        char res[length];
-        // Print "cyphertext: " now 
-        //     because I didn't find how to concatenate the cyphered characters at the end of the loop
-        printf("cyphertext: ");
-        for (int i = 0; i < length; i++) 
+        // The atoi function converts a string to an integer
+        int k = atoi(argv[1]);
+        
+        // Check if there was a second argument passed with the program call
+        //     and that that second argument is higher than 0 (has to be to cypher with caesar)
+        if (argc == 2 && k > 0)
         {
-            // Case: Char is Uppercase
-            //     ascii value of A = 65
-            if (isupper(text[i])) 
+            // Prompt user for text to cypher
+            string text = get_string("plaintext: ");
+            // Get length of the string
+            int length = strlen(text);
+            // Create an array of the same length for the ciphered characters
+            char res[length];
+            // Print "cyphertext: " now 
+            //     because I didn't find how to concatenate the ciphered characters at the end of the loop
+            printf("ciphertext: ");
+            for (int i = 0; i < length; i++) 
             {
-                res[i] = ((text[i] + k) - 65) % 25 + 65;
+                // Case: Char is Uppercase
+                //     ascii value of A = 65
+                if (isupper(text[i])) 
+                {
+                    res[i] = ((text[i] + k) - 65) % 26 + 65;
+                }
+                // Case: Char is Lowercase
+                //     ascii value of a = 97
+                else if (islower(text[i]))
+                {
+                    res[i] = ((text[i] + k) - 97) % 26 + 97;
+                }
+                // Case: not a letter of the alphabet 
+                else 
+                {
+                    res[i] = text[i];
+                }
+                // Print all the ciphered characters 
+                printf("%c", res[i]); 
             }
-            // Case: Char is Lowercase
-            //     ascii value of a = 97
-            else if (islower(text[i]))
-            {
-                res[i] = ((text[i] + k) - 97) % 25 + 97;
-            }
-            // Case: not a letter of the alphabet 
-            else 
-            {
-                res[i] = text[i];
-            }
-            // Print all the cyphered characters 
-            printf("%c", res[i]); 
+            printf("\n");
+            return 0;
+        } 
+        else 
+        {
+            printf("Usage: ./caesar key");
+            return 1;
         }
-        printf("\n");
-        return 0;
     } 
-    else 
+    else
     {
         printf("Usage: ./caesar key");
         return 1;
